4ccbbb96c4d09ead6c669f65e3855502873ee57d
ehsan <ehsanmahmodian@gmail.com>
2023-03-02 21:43:00 +0330

    add some tests for log subsystem

--- app/main.c
+++ app/main.c
@@ -3,8 +3,9 @@
 
 #define LOG_FILE_NAME "./log.txt"
 
-void simpleLogTest(void)
+void logTestDebugLvl(void)
 {
+    log_setLogLevel(LOG_LEVEL_DEBUG);//show all log messeges
     LOG_DEBUG("hello");
     LOG_INFO("hello");
     LOG_WARN("hello");
@@ -12,16 +13,58 @@ void simpleLogTest(void)
     LOG_CRITICAL("hello");
 }
 
+void logTestInfoLvl(void)
+{
+    log_setLogLevel(LOG_LEVEL_INFO);
+    LOG_DEBUG("hello");
+    LOG_INFO("hello");
+    LOG_WARN("hello");
+    LOG_ERROR("hello");
+    LOG_CRITICAL("hello");
+}
+
+void logTestWarnLvl(void)
+{
+    log_setLogLevel(LOG_LEVEL_WARN);
+    LOG_DEBUG("hello");
+    LOG_INFO("hello");
+    LOG_WARN("hello");
+    LOG_ERROR("hello");
+    LOG_CRITICAL("hello");
+}
+
+void logTestErrorLvl(void)
+{
+    log_setLogLevel(LOG_LEVEL_ERROR);
+    LOG_DEBUG("hello");
+    LOG_INFO("hello");
+    LOG_WARN("hello");
+    LOG_ERROR("hello");
+    LOG_CRITICAL("hello");
+}
+
+void logTestCriticalLvl(void)
+{
+    log_setLogLevel(LOG_LEVEL_CRITICAL);//show all log messeges
+    LOG_DEBUG("hello");
+    LOG_INFO("hello");
+    LOG_WARN("hello");
+    LOG_ERROR("hello");
+    LOG_CRITICAL("hello");
+}
 int main (int argc , char ** argv)
 {
-    log_setLogLevel(LOG_LEVEL_DEBUG);//show all log messeges 
     initilizeLogFile(LOG_FILE_NAME);
 
     //sets a callback to log subsystem for saving logs to file(with different log level)
     //logs with level warn,error and critical will save into the file.
     registerWriteToFileCb();
 
-    simpleLogTest();
-    
+    logTestDebugLvl();
+    logTestInfoLvl();
+    logTestWarnLvl();
+    logTestErrorLvl();
+    logTestCriticalLvl();
+
     return 0;
 }
